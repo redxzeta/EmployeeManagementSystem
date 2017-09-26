bec72e3ec196b62e81edaa0134e14d624bb2170d
redxzeta <xzetanzero@gmail.com>
2017-09-26 13:40:46 -0700

    added sort large to small

--- Project1/Project1/sort.cpp
+++ Project1/Project1/sort.cpp
@@ -9,6 +9,15 @@ sort::sort()
 
 void sort::sortAZ(struct adminMode adminMode[20]) {
 	cout << adminMode[0].firstName;
+
+
+		cout << "ID\tfirstname\tlastname\tEmail\t\t\tPhone Number\n";
+		for (int i = 20; i > 0; i--) {
+			cout << adminMode[i].id << "\t" << adminMode[i].firstName << "\t\t" << adminMode[i].lastName
+				<< "\t\t" << adminMode[i].email << "\t\t" << adminMode[i].number << endl;
+		}
+	
+
 }
 
 sort::~sort()
